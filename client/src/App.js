ea54477031f91969b38df7fb7178bcdfa995ae05
vshulman25 <victoriashulman25@gmail.com>
2021-05-03 22:22:07 -0400

    commiting bookshelf

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Bookshelf from "./pages/Bookshelf";
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<Router>
+{/* <Header/> */}
+<Switch>
+
+{/* Paths for pages */}
+<Route exact path = "/bookshelf" component = {Bookshelf} />
+
+
+
+
+
+
+
+
+</Switch>
+
+</Router>
+
+
+
+
+
+
+</div>
+
+
   );
 }
 
