2d5022f764ddaf2d1302b9b202eec7276bee54c9
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-05-06 19:39:27 -0400

    Classnames created

--- client/src/pages/Bookshelf.js
+++ client/src/pages/Bookshelf.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Row} from "react-bootstrap"
+import "../styles/bookshelf.css"
 
 export default class Bookshelf extends React.Component {
     render(){
@@ -9,60 +10,60 @@ export default class Bookshelf extends React.Component {
 
 {/* Currently Reading Section */}
     <Row>
-    <div className="col-12">Currently Reading</div>
+    <div className="col-12 pagetitle">Currently Reading</div>
     </Row>  
 
     <Row>
-    <div className="col-12">Book, Name current page here</div>
+    <div className="col-12 aboutme">Book, Name current page here</div>
     </Row> 
 
 
 
 {/* Want to Read Section */}
     <Row>
-    <div className="col-12">Want to Read</div>
+    <div className="col-12 subtitle">Want to Read</div>
     </Row>  
     <Row>
     <div className="col-1"></div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
     <div className="col-1"></div>
     </Row>  
 
 {/* Suggestions for you... Section */}
     <Row>
-    <div className="col-12">Suggestions for you...</div>
+    <div className="col-12 subtitle">Suggestions for you...</div>
     </Row>  
     <Row>
     <div className="col-1"></div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
     <div className="col-1"></div>
     </Row>  
 
 {/* Read Section */}
     <Row>
-    <div className="col-12">Read</div>
+    <div className="col-12 subtitle">Read</div>
     </Row>  
     <Row>
     <div className="col-1"></div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
-    <div className="col-2">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
+    <div className="col-2 book">Book Here</div>
     <div className="col-1"></div>
     </Row>  
 
 {/* search for more Section */}
 <Row>
-
+{/* search for more goes here */}
     </Row>  
 
 
