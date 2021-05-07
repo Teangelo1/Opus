2aa9d405e32fe915e967423c480b8486e87f618d
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-05-07 16:14:41 -0400

    created basic navbar layout for evenly spaced headers

---
+++ client/src/components/navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Col, Row} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import "../styles/footer.css"
+
+
+export default class Footer extends React.Component{
+
+
+    render(){
+        return(
+    
+<header>
+
+<div className="col-2"></div>
+<div className="col-4"></div>
+<div className="col-2"></div>
+<div className="col-2"></div>
+<div className="col-2"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+</header>
+    
+    
+    
+    
+    
+    )}}
