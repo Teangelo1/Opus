b77b80e7fe4c7ad2742899741e53d727bdc935dd
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-05-07 15:52:54 -0400

    created fixed footer styling

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,6 +1,30 @@
 import React from "react";
-
+import {Col, Row} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 
 
 export default class Footer extends React.Component{
+
+
+    render(){
+        return(
+    
+      <footer  style={{
+        position: "fixed",
+        bottom: "0",
+        width: "100%"}} >
+      <Row>
+
+
+
+
+
+              </Row>
+      </footer> 
+    
+    
+    
+    
+    
+    )}}
