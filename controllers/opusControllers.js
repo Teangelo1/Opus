b7e6e553aa7dca059d5ddd2151a8a3fb05f3f00e
vshulman25 <victoriashulman25@gmail.com>
2021-05-07 21:34:10 -0400

    base for routes added

---
+++ controllers/opusControllers.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Book
+      .findAll(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book
+      .findOne({where: {title: req.params.title}})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+//   create: function(req, res) {
+//     db.Book
+//       .create(req.body)
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   },
+//   update: function(req, res) {
+//     db.Book
+//       .findOneAndUpdate({ _id: req.params.id }, req.body)
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   },
+//   remove: function(req, res) {
+//     db.Book
+//       .findById({ _id: req.params.id })
+//       .then(dbModel => dbModel.remove())
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   }
+};
--- routes/api/book.js
+++ routes/api/book.js
@@ -1,12 +1,12 @@
-//const router = require("express").Router();
-//const opusController = require("../../controllers/opusControllers");
+const router = require("express").Router();
+const opusController = require("../../controllers/opusControllers");
 
 const db = require("../../models/book")
 
 // // Matches with "/api/books"
-// router.route(app)
-//   .get(opusController.findAll)
-//   .post(opusController.create); // add to a page? may not need 
+router.route("/")
+  .get(opusController.findAll)
+  .post(opusController.create); // add to a page? may not need 
 
 // // Matches with "/api/books/:id"
 // router
@@ -15,11 +15,11 @@ const db = require("../../models/book")
 //   .put(opusController.update) // would update status of reading or review
 //   .delete(opusController.remove);
 
-module.exports = (app) => {
-  app.get("/api/books", (req, res) => {
-    db.book.findAll({})
-    .then((dbRoute) => {
-      res.json(dbRoute); 
-    })
-  })
-}
+// module.exports = (app) => {
+//   app.get("/api/books", (req, res) => {
+//     db.book.findAll({})
+//     .then((dbRoute) => {
+//       res.json(dbRoute); 
+//     })
+//   })
+// }
