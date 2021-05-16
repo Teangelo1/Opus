1bada2d735f66c6128be6d1702ad6d5cd0618e4e
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-05-15 22:55:39 -0400

    styling search page

--- client/src/pages/Bookshelf.js
+++ client/src/pages/Bookshelf.js
@@ -6,80 +6,48 @@ import BookDetail from '../components/BookDisplay'
 import Header from "../components/Navbar/navbar";
 import API from '../utils/API'
 import { propTypes } from "react-bootstrap/esm/Image";
-
  function Bookshelf() {
     const [nyBooks, setNyBooks] = useState([])
     const [readBooks, setReadBooks] = useState([])
     const [wantBooks, setWantBooks] = useState([])
-    const [currentBooks, setCurrentBooks] = useState([])
-    
-    
     useEffect(() => {
         bookShelfData()
         getReadBooks()
         wantToReadBooks()
-        currentlyReading()
       }, [])
-
       function bookShelfData() {
         API.bookShelfData().then(data => { console.log(data); return data }).then(res => setNyBooks(
          res.data
         )).catch(err => console.log(err))
       } 
-
       function getReadBooks() {
         API.getReadBooks().then(data => { console.log(data); return data }).then(res => setReadBooks(
           res.data
          )).catch(err => console.log(err))
-        
       }
-
       function wantToReadBooks() {
         API.wantToReadBooks().then(data => { console.log(data); return data }).then(res => setWantBooks(
           res.data
          )).catch(err => console.log(err))
-        
       }
-<<<<<<< HEAD
-
-      function currentlyReading() {
-        API.currentlyReading().then(data => { console.log(data); return data }).then(res => setCurrentBooks(
-          res.data
-         )).catch(err => console.log(err))
-        
-      }
-
-      
-    
-    
-    
-=======
->>>>>>> b63e0944fa7bdc926c363ac74447d863a2903dc1
-     
         return (
             <div>
                 <Header></Header>
-
                 {/* Currently Reading Section */}
                 <Row className="currentlyreadingheader">
                     <div className="col-12 subtitle">Currently Reading</div>
                 </Row>
-
                 <Row className="bookreadingrow">
                     <div className="col-4"></div>
-                    <div className="col-2 favbook">{currentBooks.map((books) => (<BookDetail image={books.img} /> ))}</div>
+                    <div className="col-2 favbook">Book Here</div>
                     <div className="col-2 aboutme">Name current page here</div>
                     <div className="col-4"></div>
                 </Row>
-
-
-
                 {/* Want to Read Section */}
                 <Row>
                     <div className="col-12 subtitle">Want to Read</div>
                 </Row>
                 <Row>
-
           {/* {!nyBooks.length ? 
           (
             <h2>No Trending Books available at this moment </h2>
@@ -91,29 +59,18 @@ import { propTypes } from "react-bootstrap/esm/Image";
                   gID={`/review/${books.isbn}`}
                 />
               ))}
-
             </div> */}
           {/* } */}
-
-        
                 </Row>
-
                 {/* Suggestions for you... Section */}
-            
                 <div className="row books">
                     {wantBooks.map((books) => (<BookDetail image={books.img} gID={`/review/${books.isbn}`} /> ))}
-
-
                     </div>
-
                 {/* Read Section */}
                 <Row>
                     <div className="col-12 subtitle">Read</div>
-
                     <div className="row book">
                     {readBooks.map((books) => (<BookDetail image={books.img} /> ))}
-
-
                     </div>
                 </Row>
                 <Row>
@@ -125,11 +82,7 @@ import { propTypes } from "react-bootstrap/esm/Image";
                     <div className="col-2 book">Book Here</div>
                     <div className="col-1"></div>
                 </Row>
-
             </div>
         )
-    
-
 }
-
 export default Bookshelf
