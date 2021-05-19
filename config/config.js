164b59b1ff8f49e41d8c8679f7fbc289abbe3ed1
Teangelo Burks <burks.teangelo@gmail.com>
2021-05-18 20:59:10 -0400

    setup jawsDB

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-{
+module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": process.env.myPassword,
     "database": "opus_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
@@ -18,6 +18,7 @@
     "password": "",
     "database": "opus_db",
     "host": "127.0.0.1",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
