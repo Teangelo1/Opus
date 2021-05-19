c5f536a6d6352bf06f7c921557eff789da4e0599
vshulman25 <victoriashulman25@gmail.com>
2021-05-19 17:05:02 -0400

    tool tip added for opus league

--- client/src/pages/OpusLeague.js
+++ client/src/pages/OpusLeague.js
@@ -4,7 +4,8 @@ import { Container, Row } from "react-bootstrap";
 import Navbar from "../components/Navbar";
 import BookDetail from "../components/BookDisplay";
 import "../styles/opusleague.css";
-import ClubComments from "../components/ClubComments"; 
+import ClubComments from "../components/ClubComments";
+import ReactTooltip from "react-tooltip";
 
 
 function OpusLeague() {
@@ -23,7 +24,7 @@ function OpusLeague() {
     }
 
     function opusBookData(bookId) {
-       console.log("button value " + bookId)
+        console.log("button value " + bookId)
         API.opusLeague(bookId).then(res => {
             setOpusData(res.data)
         })
@@ -39,13 +40,13 @@ function OpusLeague() {
                     <div>
                         <Row>
 
-                          <div className="col-4">  <BookDetail gID={"/details/9781501160851"} image={"http://books.google.com/books/content?id=AIjCDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_ap"} />
-                            <button className="btn btn-light daclub" value={1} onClick={() => opusBookData(1)}>💰In Da Club🕵️‍♂️</button></div>
+                            <div className="col-4">  <BookDetail gID={"/details/9781501160851"} image={"http://books.google.com/books/content?id=AIjCDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_ap"} />
+                                <button className="btn btn-light daclub" value={1} onClick={() => opusBookData(1)} data-tip="What's the League saying?">💰In Da Club🕵️‍♂️</button></div>
                             <div className="col-4"> <BookDetail gID={"/details/9781501171345"} image={"http://books.google.com/books/content?id=3s8DEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"} />
-                            <button className="btn btn-light daclub" value={2} onClick={() => opusBookData(2)}>🕵️‍♀️In Da Club📝</button></div>
+                                <button className="btn btn-light daclub" value={2} onClick={() => opusBookData(2)} data-tip="Discover why League members love this one!">🕵️‍♀️In Da Club📝</button></div>
                             <div className="col-4"><BookDetail gID={"/details/9780765387585"} image={"http://books.google.com/books/content?id=vH3LDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"} />
-                            <button className="btn btn-light daclub" value={3} onClick={() => opusBookData(3)}>✨In Da Club🥀</button></div>
-                            
+                                <button className="btn btn-light daclub" value={3} onClick={() => opusBookData(3)} data-tip="See what the League is talking about...">✨In Da Club🥀</button></div>
+
                         </Row>
 
                     </div>
@@ -58,11 +59,11 @@ function OpusLeague() {
                                 lastName={book.User.last_name}
                                 review={book.review}
                                 stars={book.rating}
-                                />
+                            />
                         )}
 
                     </div>
-
+                    <ReactTooltip place="top" type="dark" effect="float" />
                 </Container>
             </div>
         </div>
