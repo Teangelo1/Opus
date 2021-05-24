58e43d0aa2740fb6a5a0b62ecb3b65a5d767e84c
Teangelo Burks <burks.teangelo@gmail.com>
2021-05-24 11:26:11 -0400

    Updated config.json file with env variable

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
-{
+module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": process.env.myPassword,
     "database": "opus_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "",
+    "password": process.env.myPassword,
     "database": "opus_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "",
+    "password": process.env.myPassword,
     "database": "opus_db",
     "host": "127.0.0.1",
     "use_env_variable": "JAWSDB_URL",
