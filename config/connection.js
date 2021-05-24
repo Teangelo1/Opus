7d03f7bc839fa37379fd0f93325ea9ff38ef2cf2
Teangelo Burks <burks.teangelo@gmail.com>
2021-05-24 19:33:56 -0400

    attempting to fix heroku database error

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,24 @@
 const Sequelize = require('sequelize');
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-const sequelize = new Sequelize('opus_db', 'root', process.env.myPassword, {
-  host: 'localhost',
-  port: 3306,
-  dialect: 'mysql',
-  database: 'opus_db',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-});
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+}
+
+else {
+ sequelize =  new Sequelize('opus_db', 'root', process.env.myPassword, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    database: 'opus_db',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+  });
+}
 // Exports the connection for other files to use
 module.exports = sequelize;
