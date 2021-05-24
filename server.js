a3d54f38bd5e351a12f56fe1b602d539a1fbf92d
Teangelo Burks <burks.teangelo@gmail.com>
2021-05-24 19:42:50 -0400

    updated server.js

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-if (process.env.NODE_ENV === "production") { app.use(express.static("client/build")); }
+if (process.env.NODE_ENV==='production'){
+  // set a static folder 
+  app.use(express.static('client/build'))
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 
 // Passport 
 app.use(session({ 
