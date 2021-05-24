e624138f51d9363acd43dbce2fee2e1515ce5240
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-05-24 10:06:18 -0400

    adding route for if api not hit

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ const apiRoutes = require("./api");
 // using api path
 router.use("/api", apiRoutes);
 
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+  
 module.exports = router;
